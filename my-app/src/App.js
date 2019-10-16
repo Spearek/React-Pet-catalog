1ee115ea0d25d53e492b021edf792bed214bdf8a
Spearek <30389214+Spearek@users.noreply.github.com>
2019-10-16 19:11:03 +0200

    key property added into pets State

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,20 +11,23 @@ class App extends Component {
       species: "Cat",
       favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
       birthYear: 2016,
-      photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
+      photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg",
+      key: 'dsklfjaad'
     },
     {
       name: "Barksalot",
       species: "Dog",
       birthYear: 2008,
-      photo:  "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
+      photo:  "https://learnwebcode.github.io/json-example/images/dog-1.jpg",
+      key: 'cvkcjxva'
     },
     {
       name: "Meowsalot",
       species: "Cat",
       favFoods: ["tuna", "catnip", "celery"],
       birthYear: 2012,
-      photo: "https://learnwebcode.github.io/json-example/images/cat-1.jpg"
+      photo: "https://learnwebcode.github.io/json-example/images/cat-1.jpg",
+      key: 'fskadjv'
     }]
   }
 
