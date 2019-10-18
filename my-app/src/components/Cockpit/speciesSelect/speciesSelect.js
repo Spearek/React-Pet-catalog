4c33b3b7feeb18d82881e74b23da31eaeb9bbb72
Spearek <30389214+Spearek@users.noreply.github.com>
2019-10-18 22:29:08 +0200

    Static select values created

---
+++ my-app/src/components/Cockpit/speciesSelect/speciesSelect.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const speciesSelect = () =>{
+
+    return(
+        <select value ="default">
+        <option value = "default">Gatunek</option>
+        <option value="dog">Pies</option>
+        <option value="cat">Kot</option>
+        </select>
+    )
+}
+
+
+export default speciesSelect;
