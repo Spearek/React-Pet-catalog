fa650fc70bea285a94bfccec09981463c5f959bc
Spearek <30389214+Spearek@users.noreply.github.com>
2019-10-23 12:03:50 +0200

    include key property into speciesFilter component

--- my-app/src/components/Cockpit/speciesFilter/speciesFilter.js
+++ my-app/src/components/Cockpit/speciesFilter/speciesFilter.js
@@ -4,7 +4,7 @@ import SelectedSpecies from './selectedSpecies/selectedSpecies';
 const speciesFilter = props =>{
 
     const species = props.list.map(pet=>{
-        return <SelectedSpecies pet={pet}/>
+        return <SelectedSpecies pet={pet} key={pet + '_key'}/>
     })
 
     return(
