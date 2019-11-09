1553ed5a9b3fd20827a6db6d9104531b04231e6b
Spearek <30389214+Spearek@users.noreply.github.com>
2019-11-09 18:17:40 +0100

    favFood Component created

---
+++ my-app/src/components/Pets/Pet/FavFood/FavFood.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const favFood = props => {
+
+const styles = {
+    display: 'inline',
+    border: '1px solid black',
+    padding: '2px',
+    fontSize: '14px',
+    margin: '0 10px'
+}
+
+    return <p style={styles}>{props.favorite}</p>
+    
+}
+
+export default favFood;
