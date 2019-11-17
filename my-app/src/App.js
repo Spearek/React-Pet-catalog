889ab8d28bccb1a5e01d1de75f3a27cc16b0fdd6
Spearek <30389214+Spearek@users.noreply.github.com>
2019-11-17 13:22:02 +0100

    Modal component implemented

---
+++ my-app/src/App.js
@@ -0,0 +1,96 @@
+import React, {Component} from 'react';
+import './App.css';
+import Cockpit from './components/Cockpit/Cockpit';
+import Pets from './components/Pets/Pets';
+import Modal from './components/Modal/Modal'
+
+
+class App extends Component {
+  state={
+    pets:[
+    {
+      name: "Purrsloud",
+      species: "Cat",
+      favFoods: ["wet food", "dry food", "any food"],
+      birthYear: 2016,
+      photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg",
+      key: 'dsklfjaad'
+    },
+    {
+      name: "Barksalot",
+      species: "Dog",
+      birthYear: 2008,
+      photo:  "https://learnwebcode.github.io/json-example/images/dog-1.jpg",
+      key: 'cvkcjxva'
+    },
+    {
+      name: "Meowsalot",
+      species: "Cat",
+      favFoods: ["tuna", "catnip", "celery"],
+      birthYear: 2012,
+      photo: "https://learnwebcode.github.io/json-example/images/cat-1.jpg",
+      key: 'fskadjv'
+    }],
+    speciesList: [],
+    speciesSelectVal: 'default',
+    sortedBy: {
+      birthYear: false,
+      name: false
+    }
+  }
+  componentDidMount(){
+    this.speciesListHandler();
+  }
+
+  removePetHandler = (petKey) =>{
+    const newPetArr = JSON.parse(JSON.stringify(this.state.pets));
+    const petPosition = newPetArr.findIndex(el => {   //zabezpieczyć na wypadek zwrotu -1;
+      return el.key === petKey  
+    });
+    newPetArr.splice(petPosition,1);
+    this.setState({pets: newPetArr }); // dodać komunikat gdy nie ma żadnych zwierzaków do wyświetlenia
+  }
+  
+  speciesListHandler = ()=>{
+    const list = this.state.pets.map(el =>{
+      return el.species;
+    }).reduce((acc,val)=>{
+      if(acc.indexOf(val)< 0) acc.push(val);
+      return acc;
+    },[]);
+    this.setState({speciesList:list});
+  }
+  speciesFilterHandler = (event)=>{
+    this.setState({speciesSelectVal: event.target.value});
+  }
+
+  sortHandler = (property) =>{
+    const newPetArr = JSON.parse(JSON.stringify(this.state.pets));
+    newPetArr.sort((a, b) => (a.name > b.name) ? 1 : -1);
+    if (property === 'birthYear') newPetArr.reverse();
+    this.setState({pets:newPetArr});
+  }
+
+  render(){
+
+  return (
+    <div className="App">
+      <Cockpit
+      selected={this.state.speciesSelectVal}
+      change={this.speciesFilterHandler}
+      speciesList={this.state.speciesList}
+      sort={this.sortHandler}/>
+      
+      <Pets 
+      petList={this.state.pets} 
+      click={this.removePetHandler}
+      visiblity={this.state.speciesSelectVal}/>
+
+      <Modal/>
+
+    </div>
+  );
+}
+}
+
+export default App;
