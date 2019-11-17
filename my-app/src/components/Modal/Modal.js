617e8597e78929d0ce968e4d60c4878573a0c913
Spearek <30389214+Spearek@users.noreply.github.com>
2019-11-17 15:01:18 +0100

    dynamic render of Input component implemented

--- my-app/src/components/Modal/Modal.js
+++ my-app/src/components/Modal/Modal.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import Popup from "reactjs-popup";
+import Input from "./Input/Input"
 
-const modal = () => (
-    <Popup
-      trigger={<img src={require("../../assets/photoshop slices/plus.png")}/>}
-      modal
-      closeOnDocumentClick
-    >
-      <span> Example Content </span>
-    </Popup>
-  );
+const modal = (props) => {   
+
+    const inputList = props.inputs.map(el=>{
+
+        return(
+            <Input
+             newPetData={el}
+             changed={(event)=>props.inputHandler(event,el.type)} />
+        )
+
+    })
+
+    return(
+        <Popup
+        trigger={<img src={require("../../assets/photoshop slices/plus.png")} alt="plus sign"/>}
+        modal
+        closeOnDocumentClick
+        >
+        <form>
+           {inputList} 
+        </form>
+        </Popup>
+    )};
 
   export default modal;
