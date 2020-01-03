f7c685672ff08fe18479df9f6cade5e03e43d808
Spearek <30389214+Spearek@users.noreply.github.com>
2020-01-03 22:28:03 +0100

    dynamic chance of input type

--- my-app/src/components/Modal/Input/Input.js
+++ my-app/src/components/Modal/Input/Input.js
@@ -3,7 +3,7 @@ import React from 'react';
 const input= props =>(
 
     <input 
-    type="text" 
+    type={props.newPetData.prop} 
     value={props.newPetData.value} 
     placeholder={props.newPetData.type} 
     onChange={props.changed}
