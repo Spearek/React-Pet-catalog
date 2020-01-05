27302de8ae9d52034896e9cdaa4a7d9aa1876966
Spearek <30389214+Spearek@users.noreply.github.com>
2020-01-05 16:01:40 +0100

    dynamic display properties improved

--- my-app/src/components/Modal/Radio/Radio.js
+++ my-app/src/components/Modal/Radio/Radio.js
@@ -2,16 +2,14 @@ import React from 'react';
 
 const radio = props =>{
 
-
     return(
         <label>
-            <input type="radio" value={props.speciesName} name="species"/>
+            <input type="radio" value={props.speciesName} onChange={props.changed} checked={props.speciesName === props.radioChecked}/>
             {props.speciesName}
         </label>
-
-        
 
     )
 }
 
 export default radio;
+//
