e7f2105ee036a8948b43fd01df9318db295e3b85
Spearek <30389214+Spearek@users.noreply.github.com>
2020-01-05 16:12:16 +0100

    setting pet's age ranges

--- my-app/src/components/Modal/Input/Input.js
+++ my-app/src/components/Modal/Input/Input.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-const input= props =>(
+const input= props =>{
+    
+    let youngest = new Date().getFullYear();
 
+    return(
     <input 
     type={props.newPetData.prop} 
     value={props.newPetData.value} 
     placeholder={props.newPetData.type} 
+    min={2000}
+    max={youngest}
     onChange={props.changed}
     required/>
-)
-
+)}
 
 export default input;
