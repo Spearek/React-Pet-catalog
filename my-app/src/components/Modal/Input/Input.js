dd879d395a7a8e70ef742743390fa5ad6faf0848
Spearek <30389214+Spearek@users.noreply.github.com>
2020-01-07 16:49:06 +0100

    added inline styles for paw icon

--- my-app/src/components/Modal/Input/Input.js
+++ my-app/src/components/Modal/Input/Input.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import pawIcon from '../../../assets/modal_icons/singlePaw.svg';
 
 const input= props =>{
     
     let youngest = new Date().getFullYear();
 
+    let inputStyle={
+        backgroundImage:`url(${pawIcon})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "left"
+
+    }
+
     return(
-    <input 
+    <input style={inputStyle}
     type={props.newPetData.prop} 
     value={props.newPetData.value} 
     placeholder={props.newPetData.type} 
