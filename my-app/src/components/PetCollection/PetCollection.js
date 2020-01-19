7d83c58e2dd0c279738f0d4999c05236a2e22901
Spearek <30389214+Spearek@users.noreply.github.com>
2020-01-19 17:30:44 +0100

    iniitial placeholder for PetCollection component created

---
+++ my-app/src/components/PetCollection/PetCollection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const petCollection = ()=>{
+
+    return(
+        <div>
+            <h2>Lista obserwowanych Zwierzaków</h2>
+            <p>W tym miejscu znajdzie się lista zwierzaków obserwowanych przez użytkownia.</p>
+            <p>Na ten moment jest to komponent zastępczy. Zapraszam wkrótce.</p>
+        </div>
+    )
+}
+
+export default petCollection;
