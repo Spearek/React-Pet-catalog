f741ef3c55e1d50848294030f3d8d54a0d4ba6d6
Spearek <30389214+Spearek@users.noreply.github.com>
2020-01-20 12:29:09 +0100

    MyPets component placeholder created

---
+++ my-app/src/components/MyPets/MyPets.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const myPets = ()=>{
+
+    return(
+        <div>
+            <h2>Lista dodanych Zwierzaków</h2>
+            <p>W tym miejscu znajdzie się lista zwierzaków dodanych przez użytkownika.</p>
+            <p>Na ten moment jest to komponent zastępczy. Zapraszam wkrótce.</p>
+        </div>
+    )
+}
+
+export default myPets;
