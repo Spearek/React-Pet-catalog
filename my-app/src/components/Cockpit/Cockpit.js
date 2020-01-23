d8306a493d3f1c6bcdf8adf895cdbef9406874fc
Spearek <30389214+Spearek@users.noreply.github.com>
2020-01-23 13:54:30 +0100

    Navigation component removed from Cockpit

--- my-app/src/components/Cockpit/Cockpit.js
+++ my-app/src/components/Cockpit/Cockpit.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import classes from './Cockpit.module.css';
 import SpeciesFilter from './speciesFilter/speciesFilter';
-import Navigation from './Navigation/Navigation';
-
 
 const cockpit = props =>{
     
     return(
         <div className={classes.container}>
-            <Navigation
-            modalHandler={props.modalHandler}/>
-            <div className={classes.sorts}></div>
+            
             <div className={classes.leftElements}>
                 <p>Sortowanie</p>
                 <button onClick={props.sort.bind(this,'birthYear')}>WIEK</button>
                 <button onClick={props.sort.bind(this,'name')}>IMIE</button>
             </div>
+
             <div className={classes.rightElements}>
                 <p>Filtry</p>
                 <SpeciesFilter
                 selected={props.selected}
                 change={props.change}
-                list={props.speciesList}/>
-                
+                list={props.speciesList}/>               
             </div>
         </div>
     )
