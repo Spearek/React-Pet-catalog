139dbd21fb4e6eb9b5b0b2d721e9c35a1450bf43
Spearek <30389214+Spearek@users.noreply.github.com>
2020-01-24 16:56:14 +0100

    action creators for get and store Pets from server created

---
+++ my-app/src/store/actions/actionCreators.js
@@ -0,0 +1,28 @@
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-pets';
+
+
+export const storePets =(newPets)=>{
+    return{
+        type:actionTypes.STORE_PETS,
+        newPets: newPets
+    }
+}
+
+export const storePetsASync =()=>{
+    return dispatch =>{
+        axios.get('/pets.json')
+            .then(response=>{    
+                let newPets = [];
+                for (let key in response.data){
+                    newPets.push({
+                        ...response.data[key],
+                        id: key
+                        });
+                }
+            dispatch(storePets(newPets))
+            })
+    }
+};
+
+
