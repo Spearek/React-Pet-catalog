8e1f072b81c29496729925cd71cf5031cea14f2a
Spearek <30389214+Spearek@users.noreply.github.com>
2020-01-24 16:57:24 +0100

    add logic to handler STORE_PETS actionType

--- my-app/src/store/reducer.js
+++ my-app/src/store/reducer.js
@@ -1,11 +1,19 @@
+import * as actionTypes from './actions/actionTypes';
+
 const initialState = {
+    pets: null,
     speciesList: ["Cat","Dog","Rodent"]
 }
 
 const reducer = (state=initialState, action) =>{
-
-    return state;
-
+    switch(action.type){
+        case actionTypes.STORE_PETS:
+            return{
+                ...state,
+                pets:action.newPets
+            }
+        default: return state;
+    }
 };
 
 export default reducer;
