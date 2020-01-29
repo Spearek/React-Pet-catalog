fe207f91f7469876f3b3dc82c8db22d31f371b57
Spearek <30389214+Spearek@users.noreply.github.com>
2020-01-29 19:44:09 +0100

    basename property added for better handling github pages

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -12,7 +12,7 @@ import reducer from './store/reducer';
 
 const store = createStore(reducer,applyMiddleware(ReduxThunk));
 
-ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><BrowserRouter basename="./React-Pet-catalog/"><App /></BrowserRouter></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
