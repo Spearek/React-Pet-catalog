31093a5ba963aaa7dbb38cf4fbf9c851e0d7db56
Spearek <30389214+Spearek@users.noreply.github.com>
2020-02-01 18:00:25 +0100

    action types for authorisation created

--- my-app/src/store/actions/actionTypes.js
+++ my-app/src/store/actions/actionTypes.js
@@ -2,4 +2,8 @@ export const STORE_PETS = 'STORE_PETS';
 export const LOCAL_PET_REMOVAL = 'LOCAL_PET_REMOVAL';
 export const SORT_PETS = 'SORT_PETS'
 
+export const CHANGE_AUTH_METHOD = 'CHANGE_AUTH_METHOD';
+export const AUTH_START = 'AUTH_START';
+export const AUTH_SUCCEED = 'AUTH_SUCCEED';
+export const AUTH_FAILED = 'AUTH_FAILED';
 
