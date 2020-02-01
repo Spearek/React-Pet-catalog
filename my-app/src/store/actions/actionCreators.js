ff5026336838b65fa11d9420f19b9102e3352804
Spearek <30389214+Spearek@users.noreply.github.com>
2020-02-01 18:01:25 +0100

    initial action creator for succeed authorisation created

--- my-app/src/store/actions/actionCreators.js
+++ my-app/src/store/actions/actionCreators.js
@@ -39,5 +39,45 @@ export const sortPets = (prop) =>{
     }
 }
 
+export const changeAuthMethod = ()=>{
+    return{
+        type:actionTypes.CHANGE_AUTH_METHOD
+    }
+}
+
+export const authSucceed = (email,token,userId) =>{
+    return{
+        type:actionTypes.AUTH_SUCCEED,
+        email: email,
+        token: token,
+        userId: userId
+    }
+}
+
+export const authAsync = (email,pass,haveAcc) =>{
+    return dispatch =>{
+        const userData = {
+            email:email,
+            password: pass,
+            returnSecureToken: true
+        }
+        let postURL ='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA-Wbo_JYo2XFL3YwJcVO_BUXmNemLE1aw'
+        if(!haveAcc){
+            postURL = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA-Wbo_JYo2XFL3YwJcVO_BUXmNemLE1aw'
+        } 
+        axios.post(postURL,userData)
+            .then(response=>{
+                console.log(response);
+                dispatch(authSucceed(response.data.email,response.data.idToken,response.data.localId))
+              })
+            .catch(err =>{
+                console.log(err.response.data.error);
+
+            })
+        
+
+    }
+}
+
 
 
