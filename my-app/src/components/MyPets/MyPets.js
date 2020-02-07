6a7ac5fe00884889dc52c6e60e65b7c8fe2ff13c
Spearek <30389214+Spearek@users.noreply.github.com>
2020-02-07 13:36:57 +0100

    component changed into statefull, connected into store, fetching and displaying user Pets

--- my-app/src/components/MyPets/MyPets.js
+++ my-app/src/components/MyPets/MyPets.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
+import  {connect} from 'react-redux';
+import  {fetchUserPetsAsync} from '../../store/actions/actionCreators';
+
+import Pets from '../Pets/Pets';
 
 
-const myPets = ()=>{
+class MyPets extends Component {
 
-    return(
-        <div>
-            <h2>Lista dodanych Zwierzaków</h2>
-            <p>W tym miejscu znajdzie się lista zwierzaków dodanych przez użytkownika.</p>
-            <p>Na ten moment jest to komponent zastępczy. Zapraszam wkrótce.</p>
-        </div>
-    )
+    componentDidMount(){
+        this.props.fetchPets(this.props.id);
+    }
+
+    render(){
+
+        let nonePets = <p style={{fontSize:'22px',fontWeight:'600',color:'red', marginTop:'10%'}}>Nie masz jeszcze żadnych dodanych zwierzaków.</p>;
+        if((this.props.userPets || null) && (this.props.userPets.length > 1)){
+            nonePets = null;
+        }
+
+
+        return(
+            <div>
+                <h2>Lista dodanych Zwierzaków. </h2>
+                <p>Poniżej znajduje się lista dodanych przez Ciebie zwierzaków.</p>
+                <p>W każdej chwili możesz usunąć wybranego zwierzaka z aplikacji klikając na <span>X</span></p>
+                <Pets
+                petList={this.props.userPets}
+                visiblity='default'
+                />
+                {nonePets}
+            </div>
+
+        )
+    }
 }
 
-export default myPets;
+const mapStateToProps = state =>{
+    return{
+      id: state.userId,
+      userPets: state.userPets
+    }
+  };
+
+const mapDispatchToProps = dispatch =>{
+    return{
+      fetchPets:(id)=>dispatch(fetchUserPetsAsync(id)),
+    }
+  }
+
+export default connect(mapStateToProps,mapDispatchToProps) (MyPets);
