6204051325cf6b2fa98f864da9acef9cfc1ab775
Spearek <30389214+Spearek@users.noreply.github.com>
2020-02-09 22:31:48 +0100

    component changed into statefull one, connected with store. Displaying initial favorite pets list ( list not working properly yet)

--- my-app/src/components/PetCollection/PetCollection.js
+++ my-app/src/components/PetCollection/PetCollection.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
+import  {connect} from 'react-redux';
+import  {fetchUserPetsAsync} from '../../store/actions/actionCreators';
+
+import Pets from '../Pets/Pets';
 
 
-const petCollection = ()=>{
+class PetCollection extends Component{
 
-    return(
-        <div>
-            <h2>Lista obserwowanych Zwierzaków</h2>
-            <p>W tym miejscu znajdzie się lista zwierzaków obserwowanych przez użytkownia.</p>
-            <p>Na ten moment jest to komponent zastępczy. Zapraszam wkrótce.</p>
-        </div>
-    )
+    render(){
+
+        let favoritePets = null;
+        if(this.props.allPets){
+            favoritePets = this.props.allPets.filter(el =>{
+                return el.likedBy;
+            });
+            console.log(favoritePets);
+        }
+        
+
+
+
+        return(
+            <div>
+                <h2>Lista obserwowanych Zwierzaków</h2>
+                <p>W tym miejscu znajdzie się lista zwierzaków obserwowanych przez użytkownia.</p>
+                <p>Na ten moment jest to komponent zastępczy. Zapraszam wkrótce.</p>
+                <Pets
+                petList={favoritePets}
+                userId={this.props.id}
+                visiblity='default'/>
+            </div>
+            
+        )
+    }
+
+    
 }
 
-export default petCollection;
+const mapStateToProps = state =>{
+    return{
+      id: state.userId,
+      allPets: state.pets
+    }
+  };
+
+const mapDispatchToProps = dispatch =>{
+    return{
+      fetchPets:(id)=>dispatch(fetchUserPetsAsync(id)),
+    }
+  }
+
+export default connect(mapStateToProps,mapDispatchToProps) (PetCollection);
+
+
+
+
+
+
