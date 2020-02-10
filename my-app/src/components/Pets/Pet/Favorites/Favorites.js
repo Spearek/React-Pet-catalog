2524c4d3ec61c3774624cbc27c607a70cb5302dc
Spearek <30389214+Spearek@users.noreply.github.com>
2020-02-10 21:52:00 +0100

    Like button created as own Component and connected into store

---
+++ my-app/src/components/Pets/Pet/Favorites/Favorites.js
@@ -0,0 +1,56 @@
+import React,{Component} from 'react';
+import  {connect} from 'react-redux';
+
+import emptyLike from '../../../../assets/like_icons/love_gray.svg';
+import like from '../../../../assets/like_icons/love.svg';
+import {likePetAsync} from '../../../../store/actions/actionCreators';
+
+class Favorites extends Component {
+
+    clickHandler = () =>{
+        this.props.likeClicked(this.props.token,this.props.petId,this.props.userId,this.props.petArr,this.props.userPetArr);
+    }
+
+    render(){
+
+        let iconStatus = emptyLike;
+
+        if(this.props.isLiked){
+            iconStatus = like;
+        }
+
+        const iconStyles = {
+            height: '30px',
+            idth:'30px',
+            position: 'absolute',
+            bottom: '15px',
+            right: '20px',
+            cursor: 'pointer'
+        }
+
+
+        return(
+
+            <img src={iconStatus} style={iconStyles} alt='heart icon' onClick={this.clickHandler}/>
+
+        )
+    }
+}
+
+const mapStateToProps = state =>{
+    return{
+      token: state.token,
+      isAuthenticated: state.token !== null,
+      petArr: state.pets,
+      userPetArr: state.userPets,
+      userId: state.userId
+    }
+  };
+  
+  const mapDispatchToProps = dispatch =>{
+    return{
+      likeClicked:(token,petId,userId,pets,userPets)=>dispatch(likePetAsync(token,petId,userId,pets,userPets)),
+    }
+  }
+
+export default connect(mapStateToProps,mapDispatchToProps) (Favorites);
