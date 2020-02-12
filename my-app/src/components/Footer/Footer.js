dc6372f13a94b0b2e124b5010b9d8f09cdfb70b9
Spearek <30389214+Spearek@users.noreply.github.com>
2020-02-12 12:43:44 +0100

    footer created

---
+++ my-app/src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import classes from './Footer.module.css'
+
+const footer = () => <div className={classes.footer}>Page created by Mateusz Dzida. All project Icons were created by Freepik from <a href='https://www.flaticon.com'>www.flaticon.com</a> </div>
+
+
+export default footer;
