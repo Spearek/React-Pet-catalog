66e81f8e10397ad9ef72dabf4f3a5fabcde3bec2
Spearek <30389214+Spearek@users.noreply.github.com>
2020-02-18 23:01:02 +0100

    component changed into stateless one - using react hooks

--- my-app/src/components/Auth/Logout/Logout.js
+++ my-app/src/components/Auth/Logout/Logout.js
@@ -1,17 +1,19 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import {Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import * as actions from '../../../store/actions/index';
 
-class Logout extends Component{
+const Logout = props =>{
 
-    componentDidMount(){
-        this.props.userLogout();
-    }
+    const {userLogout} = props;
 
-    render(){
-        return <Redirect to='/'/>
-    }
+    useEffect(()=>{
+        userLogout();
+    },[userLogout])
+
+    return <Redirect to='/'/>
+     
+    
 }
 
 const mapDispatchToProps = dispatch =>{
