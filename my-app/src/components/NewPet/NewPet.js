6e662a0450295f9500c544706ade25eb5a97bc9c
Spearek <30389214+Spearek@users.noreply.github.com>
2020-03-04 13:15:23 +0100

    bug fixed - add placeholder when new Pet url is too short

--- my-app/src/components/NewPet/NewPet.js
+++ my-app/src/components/NewPet/NewPet.js
@@ -13,6 +13,9 @@ import dogIcon from "../../assets/modal_icons/dog.svg";
 import hamsterIcon from "../../assets/modal_icons/hamster.svg";
 import pawsIcon from "../../assets/modal_icons/paws.svg";
 import plusIcon from "../../assets/photoshop slices/plus.png";
+import placeholder_Cat from "../../assets/placeholders/placeholder_cat.svg";
+import placeholder_Dog from "../../assets/placeholders/placeholder_dog.svg";
+import placeholder_Rodent from "../../assets/placeholders/placeholder_rodent.svg";
 import * as actions from '../../store/actions/index';
 
 
@@ -78,6 +81,21 @@ const NewPet = props => {
             photo: newPet[3].value,
             addedBy: props.user
             }
+        if (adopted.photo.length <= 10) {
+            switch (adopted.species) {
+                case 'Cat':
+                  adopted.photo = placeholder_Cat;
+                  break;
+                case 'Dog':
+                  adopted.photo = placeholder_Dog;
+                  break;
+                case 'Rodent':
+                  adopted.photo = placeholder_Rodent;
+                  break;
+                default:
+                  alert( "Should't reach that. Please refresh App" );
+              }
+        }
         
         props.addPet(adopted,props.token,props.haveErr);
 
